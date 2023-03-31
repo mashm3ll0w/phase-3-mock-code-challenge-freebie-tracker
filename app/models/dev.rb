be6fc0d3e20de67c3c081db72cf1da0393d07a17
Charles Swaleh <charlswall@live.com>
2023-03-31 21:21:48 +0300

    Completed the received_one? instance method for the Dev class

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,4 +1,8 @@
 class Dev < ActiveRecord::Base
   has_many :freebies
   has_many :companies, through: :freebies
+
+  def received_one?(item_name)
+    self.freebies.map { |freebie| freebie.item_name }.include?(item_name)
+  end
 end
