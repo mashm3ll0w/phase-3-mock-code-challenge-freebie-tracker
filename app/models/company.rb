0c54429f27fb5267581de6dfa164f92edad6f8ba
Charles Swaleh <charlswall@live.com>
2023-03-31 20:59:43 +0300

    Created the give_freebie instance method for the Company class

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,13 @@
 class Company < ActiveRecord::Base
   has_many :freebies
   has_many :devs, through: :freebies
+
+  def give_freebie(dev, item_name, value)
+    Freebie.create(
+      item_name: item_name,
+      value: value,
+      company_id: self.id,
+      dev_id: dev.id
+    )
+  end
 end
